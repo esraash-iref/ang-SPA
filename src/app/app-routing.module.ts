7fb76b11f241c82dbd2ea819d9334dcc10a00a01
esraash-iref <esraashiref504@gmail.com>
2024-12-27 21:16:37 +0200

    firist version

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { CoursesComponent } from './courses/courses.component';
+import { TrainersComponent } from './trainers/trainers.component';
+import { EventsComponent } from './events/events.component';
+import { PricingComponent } from './pricing/pricing.component';
+import { ContactComponent } from './contact/contact.component';
+import { NotfoundComponent } from './shared/notfound/notfound.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"" , component:HomeComponent},
+  {path:"home" , component: HomeComponent},
+  {path:"about", component: AboutComponent},
+  {path:"courses", component: CoursesComponent},
+  {path:"trainers", component: TrainersComponent},
+  {path:"events",component: EventsComponent},
+  {path:"pricing",component: PricingComponent},
+  {path:"contact",component: ContactComponent},
+  {path:"**" , component: NotfoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,32 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { AboutComponent } from './about/about.component';
+import { CoursesComponent } from './courses/courses.component';
+import { TrainersComponent } from './trainers/trainers.component';
+import { EventsComponent } from './events/events.component';
+import { PricingComponent } from './pricing/pricing.component';
+import { ContactComponent } from './contact/contact.component';
+import { NotfoundComponent } from './shared/notfound/notfound.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent,
+    AboutComponent,
+    CoursesComponent,
+    TrainersComponent,
+    EventsComponent,
+    PricingComponent,
+    ContactComponent,
+    NotfoundComponent,
+    
   ],
   imports: [
     BrowserModule,
